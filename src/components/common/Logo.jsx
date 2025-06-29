f41258973ac7b65a0dfb10b89e38dbe37067520b
Surendar Singh Bhandari <surendar.bhandari@sifycorp.com>
2025-06-29 20:11:31 +0530

    com1vv

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from '../../logo.svg'; // adjust path if needed
 
 const Logo = () => (
-  <img src={logo} alt="MKN Infotech Logo" className="w-101 h-auto" />
+  <img src={logo} alt="MKN Infotech Logo" className="w-80 h-auto" />
 );
 
 export default Logo;
